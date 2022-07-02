7a6581e252e5f3337bf67427fa522efa126b4d98
git_practice_again <hasanmehedi4484@gmail.com>
2022-07-03 00:42:57 +0600

    intorduction to DSA

---
+++ 01_course_overview/3_basic_oop.c
@@ -0,0 +1,12 @@
+/*
+1. high level laguage
+2. mid level language
+3. low level language
+
+object oriented programming
+1. Class and Object
+2. Inheritance
+3. Encapsulation
+4. Polymorphism
+5. Abstraction
+*/
---
+++ 01_course_overview/6_inheritance.c
@@ -0,0 +1,9 @@
+/*
+1. common
+2. specific
+
+1. public
+2. protected
+3. private
+
+*/
