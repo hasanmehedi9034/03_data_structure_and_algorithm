f2036b49c2449bd7bb5eae0c8843eaeacd22524b
git_practice_again <hasanmehedi4484@gmail.com>
2022-07-18 12:02:25 +0600

    string element access at, front, back

---
+++ 06_string_in_c++/4_string_element_access.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string st = "abcdefghij";
+    cout << st.at(3) << endl;
+    cout << st.front() << endl;
+    cout << st.back() << endl;
+}
