0a0ad63e8181251320de4249ce1aa510e9175a4a
git_practice_again <hasanmehedi4484@gmail.com>
2022-07-21 16:34:53 +0600

    bank account creat and show balance

--- 07_lav_class_02/practice.cpp
+++ 07_lav_class_02/practice.cpp
@@ -19,9 +19,30 @@ class BankAccount {
             this->age = age;
             this->password = password;
             this->account_number = rand() % 1000000000;
+            this->balance = 0;
+        }
+        int show_balance(string password) {
+            if (password == this->password) return this->balance;
+            return -1;
         }
 };
 
-int main() {
+BankAccount *create_account () {
+    string account_holder;
+    string address;
+    string password;
+    int age;
 
+    cout << "CREATE ACCOUNT : NAME(string), ADDRESS(string), AGE(age), PASSWORD(string)" << endl;
+    cin >> account_holder >> address >> age >> password;
+
+    BankAccount *my_account = new BankAccount(account_holder, address, age, password);
+    return my_account;
+}
+
+int main() {
+    BankAccount *myAccount = create_account();
+    
+    // if (!myAccount->show_balance("mehedi") == -1) cout << "password didnt match" << endl;
+    // else cout << "Your balance is : " << myAccount->show_balance("mehedi") << endl;
 }
