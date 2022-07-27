34525a9189e50de23c4f1771387a61b2d6098ac0
git_practice_again <hasanmehedi4484@gmail.com>
2022-07-27 11:22:25 +0600

    optimize bubble sort

--- 11_lav_class_03/2_bubble_sort.cpp
+++ 11_lav_class_03/2_bubble_sort.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print_array (int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
 void bubble(int a[], int len_a) {
     for (int i = 0; i < len_a - 1; i++) {
         for (int j = i + 1; j < len_a; j++) {
@@ -13,23 +20,19 @@ void bubble(int a[], int len_a) {
 
 void bubble2(int a[], int n) {
     for (int i = 1; i < n; i++) {
-        for (int j = 0; j < n - 1; j++) {
+        int flag = 0;
+        for (int j = 0; j < n - i; j++) {
             if (a[j] > a[j + 1]) {
                 swap(a[j], a[j + 1]);
+                flag = 1;
             }
         }
+        if (flag == 0) break;
     }
-}
-
-void print_array (int a[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
 }
 
 int main() {
-    int a[] = {14, 11, 1, 5, 4, 0};
+    int a[] = {1, 3, 4, 5, 6};
     int len_a = sizeof(a) / sizeof(a[0]);
 
     cout << "Before sort: ";
