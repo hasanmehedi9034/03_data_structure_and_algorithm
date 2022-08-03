75dce65be4f80bf528af4033653d6b2e78bbaeb2
git_practice_again <hasanmehedi4484@gmail.com>
2022-08-03 23:13:57 +0600

    insertin at tail

---
+++ 15_linked_list/5_upgrade_linked_list.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *next;
+
+        // constructor creation
+        Node(int val) {
+            this->value = val;
+            this->next = NULL;
+        }
+};
+
+void display(Node *n) {
+    while(n != NULL) {
+        cout << n->value;
+        if (n->next != NULL) cout << "--> ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+void insert_at_tail(Node* &head, int value) {
+    Node *new_node = new Node(value);
+    
+    if (head == NULL) {
+        head = new_node;
+        return;
+    }
+
+    Node *temp = head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+}
+
+int main() {
+    Node *head = NULL;
+
+    int n;
+    cout << "Enter the value: ";
+    cin >> n;
+    
+    char choice;
+    while(choice == 'Y') {
+        insert_at_tail(head, n);
+
+        cout << "Do you want to continue: (Y/N) ";
+        cin >> choice;
+    }
+
+    display(head);
+}
