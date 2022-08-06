f8cac32933b17ec90504e6b4cbcc806bddfcfe9d
git_practice_again <hasanmehedi4484@gmail.com>
2022-08-06 10:20:26 +0600

    counting sort with step by step

--- 16_theory_exam/3.cpp
+++ 16_theory_exam/3.cpp
@@ -46,14 +46,24 @@ int main() {
     int a[] = {7, 2, 9, 2, 7, 4, 5, 1, 0};
     int len_a = sizeof(a) / sizeof(a[0]);
 
+    // step 1: find max from the main given array, and make array length of counter array.
     int len_counter = find_max(a, len_a) + 1;
+
+    // step 2: make ready counter array
     int counter[len_counter];
 
+    // step 3: update counter array
     update_counter(a, len_a, counter, len_counter);
+
+    // step 4: update counter with prefix sum
     q_sum(counter, len_counter);
 
+    // step 5.1: make final array which contain sorting element
     int final_array[len_a];
 
+    //step 5.2: sorting array
     counting_sort(a, len_a, counter, final_array);
+
+    // print array after sorting
     print_array(final_array, len_a);
 }
