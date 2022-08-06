969e61201bdc0440e33d0de8f53a07dc191bfdf6
git_practice_again <hasanmehedi4484@gmail.com>
2022-08-06 10:42:14 +0600

    linear linked list

---
+++ 16_theory_exam/9.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *next;
+        Node (int val) {
+            this->value = val;
+            this->next = NULL;
+        }
+};
+
+int main() {
+    Node *first = new Node(5);
+    Node *second = new Node(7);
+    Node *third = new Node(1);
+    Node *fourth = new Node(14);
+    Node *fifth = new Node(3);
+    Node *sixth = new Node(11);
+
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = NULL;
+
+    int sum = 0;
+    Node *temp = first;
+
+    while(temp->value != 3) {
+        sum += temp->value;
+        temp = temp->next;
+    }
+
+    sum -= temp->value;
+    cout << sum;
+}
+
+
+// All Answer;
+/*
+    1. Value of Head is 5;
+    2. 1020 is In place of Question(?) markl
+    3. 1 is the value of Head->Next->Next->Value.
+    4. 24 is total sum value.
+*/
