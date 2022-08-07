58ffb0bbcc6f42112c300db0d39606179d2bff65
git_practice_again <hasanmehedi4484@gmail.com>
2022-08-08 00:33:24 +0600

    return array search duplicate value

---
+++ 18_linked_list_operation_2/1_search_duplicate_value.cpp
@@ -0,0 +1,190 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *next;
+        Node (int val){
+            this->value = val;
+            this->next = NULL;
+        }
+};
+
+struct Test{
+    int position[1000];
+};
+
+void print_linked_list(Node *head);
+void insert_at_head(Node* &head, int val);
+void insert_at_tail(Node* &head, int val);
+int list_size(Node *head);
+void insert_at_specific_position(Node* &head, int pos, int value);
+int search_by_value_unique(Node* head, int key);
+void search_by_value_duplicate(Node* head, int key);
+int* search_duplicate(Node *head, int key);
+Test search_by_val_dup(Node *head, int key);
+
+void print_linked_list(Node *head) {
+    cout << "[";
+    while(head != NULL) {
+        cout << head->value;
+        if (head->next != NULL) cout << ", ";
+        head = head->next;
+    }
+    cout << "]";
+    cout << endl;
+}
+
+void insert_at_head(Node* &head, int val) {
+    Node *new_node = new Node(val);
+
+    if (head == NULL) {
+        head = new_node;
+        return;
+    }
+
+    new_node->next = head;
+    head = new_node;
+}
+
+void insert_at_tail(Node* &head, int val) {
+    Node *new_node = new Node(val);
+
+    if (head == NULL) {
+        head = new_node;
+        return;
+    }
+
+    Node *temp = head;
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+}
+
+int list_size(Node *head) {
+    int count = 0;
+    while(head != NULL) {
+        head = head->next;
+        count++;
+    }
+    return count;
+}
+
+void insert_at_specific_position(Node* &head, int pos, int value) {
+    Node *new_node = new Node(value);
+
+    if (head == NULL) {
+        cout << "The list is Empty. if you insert a value, the is inserted at head position" << endl;
+        head = new_node;
+        return;
+    }
+
+    Node *temp = head;
+    int count = 1;
+    while(count < pos - 1) {
+        temp = temp->next;
+        count++;
+    }
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+}
+
+int search_by_value_unique(Node* head, int key) {
+    int position = 1;
+
+    while(head != NULL) {
+        if (head->value == key) return position;
+        head = head->next;
+        position++;
+    }
+    return -1;
+}
+
+void search_by_value_duplicate(Node* head, int key) {
+    int position = 1;
+    bool is_found = false;
+
+    while(head != NULL) {
+        if (head->value == key) {
+            cout << position << " ";
+            is_found = true;
+        }
+        head = head->next;
+        position++;
+    }
+    if(!is_found) cout << "The search value is not yet in this list" << endl;
+}
+
+int* search_duplicate(Node *head, int key) {
+    int *ar = new int[list_size(head) + 1];
+    int arr_index = 1;
+
+    int position = 1;
+    bool is_found = false;
+
+    while(head != NULL) {
+        if (head->value == key) {
+            is_found = true;
+            ar[arr_index] = position;
+            arr_index++;
+        }
+        head = head->next;
+        position++;
+    }
+
+    if (!is_found) {
+        ar[0] = -1;
+        return ar;
+    }
+    else {
+        ar[0] = arr_index;
+        return ar;
+    }
+}
+
+Test search_by_val_dup(Node *head, int key) {
+    Node *temp = head;
+    int k = 1;
+    int count = 1;
+
+    Test T;
+    while (temp != NULL) {
+        if (temp->value == key) {
+            T.position[k] = count;
+            k++;
+        }
+        temp = temp->next;
+        count++;
+    }
+    T.position[0] = k;
+    return T;
+}
+
+int main() {
+    Node *head = NULL;
+
+    insert_at_head(head, 1);
+    insert_at_head(head, 1);
+    insert_at_head(head, 1);
+    insert_at_head(head, 2);
+    insert_at_head(head, 3);
+    insert_at_tail(head, 10);
+    insert_at_tail(head, 1);
+
+    print_linked_list(head);
+
+    Test T = search_by_val_dup(head, 1);
+    if (T.position[0] == 1) cout << "Not Found" << endl;
+    else {
+        for (int i = 1; i < T.position[0]; i++) {
+            cout << T.position[i];
+            if (i < T.position[0] - 1) cout << ", ";
+        }
+        cout << endl;
+    }
+    cout << "total list length: " << list_size(head) << endl;
+}
