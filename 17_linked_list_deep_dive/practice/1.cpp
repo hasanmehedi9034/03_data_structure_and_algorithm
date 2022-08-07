77278e5f16105511b20b4aaeb61b2bdf085d00cd
git_practice_again <hasanmehedi4484@gmail.com>
2022-08-07 13:57:02 +0600

    practice-17, insert_head, print_list

---
+++ 17_linked_list_deep_dive/practice/1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *next;
+        Node (int val){
+            this->value = val;
+            this->next = NULL;
+        }
+};
+
+void print_linked_list(Node *head);
+void insert_at_head(Node* &head, int val);
+
+void print_linked_list(Node *head) {
+    cout << "[";
+    while(head != NULL) {
+        cout << head->value;
+        if (head->next != NULL) cout << ", ";
+        head = head->next;
+    }
+    cout << "]";
+    cout << endl;
+}
+
+void insert_at_head(Node* &head, int val) {
+    Node *new_node = new Node(val);
+
+    if (head == NULL) {
+        head = new_node;
+        return;
+    }
+
+    new_node->next = head;
+    head = new_node;
+}
+
+int main() {
+    Node *head = NULL;
+
+    insert_at_head(head, 1);
+    insert_at_head(head, 2);
+    insert_at_head(head, 3);
+    print_linked_list(head);
+}
