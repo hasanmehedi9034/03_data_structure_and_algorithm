76aafa35e9048238d582f9d11c0c48b39d35aded
hasanmehedi9034 <mehedi90344@gmail.com>
2022-08-29 14:23:31 +0600

    implementation tree

---
+++ 29_binary_tree(1)/1_binary_tree.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class treeNode {
+    public:
+        int data;
+        treeNode* leftChild;
+        treeNode* rightChild;
+
+        treeNode(int value) {
+            this->data = value;
+            this->leftChild = NULL;
+            this->rightChild = NULL;
+        }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    treeNode* allNodes[n];
+    for (int i = 0; i < n; i++) {
+        allNodes[i] = new treeNode(-1);
+    }
+
+    for (int i = 0; i < n; i++) {
+        int value, left, right;
+        cin >> value >> left >> right;
+
+        allNodes[i]->data = value;
+
+        if (left != -1) {
+            allNodes[i]->leftChild = allNodes[left];
+        }
+
+        if (right != -1) {
+            allNodes[i]->rightChild = allNodes[right];
+        }
+    }
+}
+
+
+/*
+9
+0 1 2
+1 3 4
+2 5 6
+3 -1 -1
+4 -1 -1
+5 7 8
+6 -1 -1
+7 -1 -1
+8 -1 -1
+
+
+
+Root: 0
+Left:
+    Root: 1
+    Left: 3
+    Right: 4
+Right: 
+    Root: 2
+    Left:
+        Root: 5
+        Left: 7
+        Right: 8
+    Right: 6
+
+
+
+*/
