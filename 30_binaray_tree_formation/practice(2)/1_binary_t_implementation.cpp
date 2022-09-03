dc711cd3d531ae9dbbd8abf953ff06f836d2c75e
hasanmehedi9034 <mehedi90344@gmail.com>
2022-09-03 12:22:19 +0600

    recap, print tree

--- 30_binaray_tree_formation/practice(2)/1_binary_t_implementation.cpp
+++ 30_binaray_tree_formation/practice(2)/1_binary_t_implementation.cpp
@@ -14,13 +14,48 @@ class treeNode {
         }
 };
 
+void printTree(treeNode* root, int level);
+void spacePrint(int level);
+
+void printTree(treeNode* root, int level) {
+    if (root == NULL) return;
+
+    if (root->leftChild == NULL && root->rightChild == NULL) {
+        cout << root->data;
+    }
+    else {
+        cout << endl;
+        spacePrint(level);
+        cout << "Root: " << root->data << endl;
+    }
+
+    if (root->leftChild != NULL) {
+        spacePrint(level);
+        cout << "Left: ";
+        printTree(root->leftChild, level + 1);
+        cout << endl;
+    }
+
+    if (root->rightChild != NULL) {
+        spacePrint(level);
+        cout << "Right: ";
+        printTree(root->rightChild, level + 1);
+    }
+}
+
+void spacePrint(int level) {
+    for (int i = 0; i < level; i++) {
+        cout << "   ";
+    }
+}
+
 int main() {
     int n;
     cin >> n;
 
     treeNode* allNodes[n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++){
         allNodes[i] = new treeNode(-1);
     }
 
@@ -38,6 +73,8 @@ int main() {
             allNodes[i]->rightChild = allNodes[right];
         }
     }
+
+    printTree(allNodes[0], 0);
 }
 
 /*
