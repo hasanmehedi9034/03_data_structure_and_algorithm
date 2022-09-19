9ec1c3b7c79d9563777ea8c742a863ff3c87048c
hasanmehedi9034 <mehedi90344@gmail.com>
2022-09-19 12:01:30 +0600

    implemetion of BST

---
+++ 36_binary_search_tree/practice(1)/0_practice.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class treeNode {
+    public:
+        int data;
+        treeNode* leftChild;
+        treeNode*  rightChild;
+
+        treeNode(int value) {
+            this->data = value;
+            this->leftChild = NULL;
+            this->rightChild = NULL;
+        }
+};
+
+void inOrder(treeNode* root, string &chk) {
+    if (root == NULL) return;
+
+    inOrder(root->leftChild, chk);
+    chk += to_string(root->data) + " ";
+    inOrder(root->rightChild, chk);
+}
+
+treeNode* bst(treeNode* root, int value) {
+    treeNode* newNode = new treeNode(value);
+
+    if (root == NULL) {
+        root = newNode;
+        return root;
+    } 
+
+    if (root->data > value) {
+        root->leftChild = bst(root->leftChild, value);
+    }
+    else if (root->data < value) {
+        root->rightChild = bst(root->rightChild, value);
+    }
+
+    return root;
+}
+
+void spacePrint(int level){
+    for (int i = 0; i < level; i++) {
+        cout << "   ";
+    }
+}
+
+void printTree(treeNode* root, int level) {
+    if (root == NULL) return;
+
+    if (root->leftChild == NULL && root->rightChild == NULL) {
+        cout << root->data << endl;
+        return;
+    }
+    else {
+        cout << endl;
+        spacePrint(level);
+        cout << "Root: " << root->data << endl;
+    }
+
+    if (root->leftChild != NULL) {
+        spacePrint(level);
+        cout << "Left: ";
+        printTree(root->leftChild, level + 1);
+    }
+    if (root->rightChild != NULL) {
+        spacePrint(level);
+        cout << "Right: ";
+        printTree(root->rightChild, level + 1);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    treeNode* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+
+        root = bst(root, value);
+    }
+
+    // string inO = "";
+    // inOrder(root, inO);
+    // cout << inO;
+
+    printTree(root, 0);
+}
+
+
+/*
+
+9
+11 5 9 43 34 1 2 7 21
+
+*/
---
+++ 36_binary_search_tree/practice(1)/2_implement_BST.cpp
@@ -0,0 +1,214 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class treeNode {
+    public:
+        int data;
+        treeNode* leftChild;
+        treeNode*  rightChild;
+
+        treeNode(int value) {
+            this->data = value;
+            this->leftChild = NULL;
+            this->rightChild = NULL;
+        }
+};
+
+void print_tree(treeNode* root, int level);
+void inOrder(treeNode* root, string &chk);
+void preOrder(treeNode* root, string &chk);
+treeNode* buildTreePreIn(int preOrder[], int inOrder[], int start, int end);
+void boundaryTraversal(treeNode* root);
+treeNode* insertionBST(treeNode* root, int value);
+ 
+void spacePrint(int level) {
+    for (int i = 0; i < level; i++) {
+        cout << "   ";
+    }
+}
+
+void print_tree(treeNode* root, int level) {
+    if (root == NULL) return;
+
+    if (root->leftChild == NULL && root->rightChild == NULL) {
+        cout << root->data << endl;
+        return;
+    }
+    else {
+        cout << endl;
+        spacePrint(level);
+        cout << "Root: " << root->data << endl;
+    }
+
+    if (root->leftChild != NULL) {
+        spacePrint(level);
+        cout << "Left: ";
+        print_tree(root->leftChild, level + 1);
+    }
+
+    if (root->rightChild != NULL) {
+        spacePrint(level);
+        cout << "Right: ";
+        print_tree(root->rightChild, level + 1);
+    }
+}
+
+void print_tree2(treeNode* root, int level) {
+    if (root == NULL) return;
+
+    if (root->leftChild == NULL && root->rightChild == NULL) {
+        cout << root->data;
+        return;
+    }
+    else {
+        cout << endl;
+        spacePrint(level);
+        cout << "Root: " << root->data << endl;
+    }
+
+    cout << "Left: ";
+    print_tree(root->leftChild, level + 1);
+
+    cout << "Right: ";
+    print_tree(root->rightChild, level + 1);
+}
+
+void inOrder(treeNode* root, string &chk) {
+    if (root == NULL) return;
+
+    inOrder(root->leftChild, chk);
+    chk += to_string(root->data) + " ";
+    inOrder(root->rightChild, chk);
+}
+
+void preOrder(treeNode* root, string &chk) {
+    if (root == NULL) return;
+
+    chk += to_string(root->data);
+    preOrder(root->leftChild, chk);
+    preOrder(root->rightChild, chk);
+}
+
+int searcInorder(int inOrder[], int current, int start, int end) {
+    for (int i = start; i <= end; i++) {
+        if (inOrder[i] == current) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+treeNode* buildTreePreIn(int preOrder[], int inOrder[], int start, int end) {
+    static int id = 0;
+
+    int current = preOrder[id];
+    treeNode* newNode = new treeNode(current);
+
+    id++;
+
+    if (start == end) {
+        return newNode;
+    }
+
+    int pos = searcInorder(inOrder, current, start, end);
+
+    newNode->leftChild = buildTreePreIn(preOrder, inOrder, start, pos - 1);
+    newNode->rightChild = buildTreePreIn(preOrder, inOrder, pos + 1, end);
+
+    return newNode;
+}
+
+void printLeaves(treeNode* root) {
+    if (root == NULL) return;
+
+    if (root->leftChild == NULL && root->rightChild == NULL) {
+        cout << root->data << " ";
+        return;
+    }
+
+    printLeaves(root->leftChild);
+    printLeaves(root->rightChild);
+}
+
+void  printLeftNonLeaves(treeNode* root) {
+    if (root == NULL) return;
+
+    if (root->leftChild != NULL) {
+        cout << root->data << " ";
+        printLeftNonLeaves(root->leftChild);
+    }
+    else if (root->rightChild != NULL) {
+        cout << root->data << " ";
+        printLeftNonLeaves(root->rightChild);
+    }
+}
+
+void printRightNonLeaves(treeNode* root) {
+    if (root == NULL) return;
+
+    if (root->rightChild != NULL) {
+        cout << root->data << " ";
+        printRightNonLeaves(root->rightChild);
+    }
+    else if (root->leftChild != NULL) {
+        cout << root->data << " ";
+        printRightNonLeaves(root->leftChild);
+    }
+}
+
+void boundaryTraversal(treeNode* root) {
+    if (root == NULL) return;
+
+    cout << root->data << " ";
+
+    printLeftNonLeaves(root->leftChild);
+
+    printLeaves(root->leftChild);
+
+    printLeaves(root->rightChild);
+
+    printRightNonLeaves(root->rightChild);
+}
+
+treeNode* insertionBST(treeNode* root, int value) {
+    treeNode* newNode = new treeNode(value);
+
+    if (root == NULL) {
+        root = newNode;
+        return root;
+    }
+
+    if (value < root->data) {
+        root->leftChild = insertionBST(root->leftChild, value);
+    }
+    else if (value > root->data) {
+        root->rightChild = insertionBST(root->rightChild, value);
+    }
+
+    return root;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    treeNode* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+
+        root =  insertionBST(root, value);
+    }
+
+    string inO = "";
+    inOrder(root, inO);
+    cout << inO;
+}
+
+/*
+
+9
+11 5 9 43 34 1 2 7 21
+
+*/
